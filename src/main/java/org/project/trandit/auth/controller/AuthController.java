fca91ac80e4c21deae33ea4436a55b0a13dff861
jeong eun <lje7370@gmail.com>
2025-07-11 15:32:13 +0900

    feat:AuthUtils_컴포넌트_생성

--- src/main/java/org/project/trandit/auth/controller/AuthController.java
+++ src/main/java/org/project/trandit/auth/controller/AuthController.java
@@ -128,7 +128,5 @@ public class AuthController {
 
         return ResponseEntity.ok(Map.of("message", "로그아웃 되었습니다."));
     }
-
-
-
+    
 }
---
+++ src/main/java/org/project/trandit/global/util/AuthUtils.java
@@ -0,0 +1,43 @@
+package org.project.trandit.global.util;
+
+import org.project.trandit.domain.member.Member;
+import org.project.trandit.security.CustomUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class AuthUtils {
+
+    // 현재 인증된 사용자 정보 반환
+    public static CustomUserDetails getCurrentUserDetails(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated() && authentication.getPrincipal() instanceof CustomUserDetails){
+            return (CustomUserDetails) authentication.getPrincipal();
+        }
+        return null;
+    }
+    // 현재 로그인한 Member 반환
+    public static Member getCurrentMember(){
+        CustomUserDetails userDetails = getCurrentUserDetails();
+        return userDetails != null ? userDetails.getMember() : null;
+    }
+
+    // 로그인한 사용자의 email 반환
+    public static String getCurrentUserEmail(){
+        Member member = getCurrentMember();
+        return member != null ? member.getEmail() : null;
+    }
+
+    // 로그인 여부 확인
+    public static boolean isLoggedIn(){
+        return getCurrentUserDetails() != null;
+    }
+
+    //현재 사용자 Role 반환
+    public static String getCurrentUserRole(){
+        Member member = getCurrentMember();
+        return member != null ? member.getRole().name() : null;
+    }
+
+
+}
